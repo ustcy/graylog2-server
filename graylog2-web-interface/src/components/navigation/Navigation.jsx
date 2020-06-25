22363f03e1ed5be8a627111ba48e6ab8afac88d6
Kyle Knight <kyle@graylog.com>
2020-06-25 07:01:52 -0500

    logo

--- graylog2-web-interface/src/components/navigation/Navigation.jsx
+++ graylog2-web-interface/src/components/navigation/Navigation.jsx
@@ -28,11 +28,11 @@ import StyledNavbar from './Navigation.styles';
 const CurrentUserStore = StoreProvider.getStore('CurrentUser');
 
 const _isActive = (requestPath, prefix) => {
-  return requestPath.indexOf(URLUtils.appPrefixed(prefix)) === 0;
+  return requestPath.indexOf(appPrefixed(prefix)) === 0;
 };
 
 const formatSinglePluginRoute = ({ description, path, permissions }, topLevel = false) => {
-  const link = <NavigationLink key={description} description={description} path={URLUtils.appPrefixed(path)} topLevel={topLevel} />;
+  const link = <NavigationLink key={description} description={description} path={appPrefixed(path)} topLevel={topLevel} />;
 
   if (permissions) {
     return <IfPermitted key={description} permissions={permissions}>{link}</IfPermitted>;
@@ -78,7 +78,7 @@ const Navigation = ({ permissions, fullName, location, loginName }) => {
   const pluginItems = PluginStore.exports('navigationItems');
 
   return (
-    <StyledNavbar inverse fluid fixedTop>
+    <StyledNavbar fluid fixedTop>
       <Navbar.Header>
         <Navbar.Brand>
           <LinkContainer to={Routes.STARTPAGE}>
