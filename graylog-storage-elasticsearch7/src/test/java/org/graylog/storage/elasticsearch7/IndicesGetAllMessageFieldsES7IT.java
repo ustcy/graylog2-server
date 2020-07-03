a27462a0ca5736c2d35de495da1e07e29a6da309
Dennis Oelkers <dennis@graylog.com>
2020-07-03 16:53:17 +0200

    Implementing field retrieval.

--- graylog-storage-elasticsearch7/src/test/java/org/graylog/storage/elasticsearch7/IndicesGetAllMessageFieldsES7IT.java
+++ graylog-storage-elasticsearch7/src/test/java/org/graylog/storage/elasticsearch7/IndicesGetAllMessageFieldsES7IT.java
@@ -1,38 +1,24 @@
 package org.graylog.storage.elasticsearch7;
 
 import com.fasterxml.jackson.databind.ObjectMapper;
-import com.google.common.collect.ImmutableMap;
 import org.graylog.storage.elasticsearch7.cat.CatApi;
 import org.graylog.storage.elasticsearch7.cluster.ClusterStateApi;
 import org.graylog.storage.elasticsearch7.stats.StatsApi;
 import org.graylog.storage.elasticsearch7.testing.ElasticsearchInstanceES7;
 import org.graylog.testing.elasticsearch.ElasticsearchInstance;
 import org.graylog2.indexer.indices.IndicesAdapter;
-import org.graylog2.indexer.indices.IndicesIT;
+import org.graylog2.indexer.indices.IndicesGetAllMessageFieldsIT;
 import org.graylog2.shared.bindings.providers.ObjectMapperProvider;
-import org.junit.Before;
 import org.junit.Rule;
 
-import java.util.Map;
-
-public class IndicesES7IT extends IndicesIT {
+public class IndicesGetAllMessageFieldsES7IT extends IndicesGetAllMessageFieldsIT {
     @Rule
     public final ElasticsearchInstanceES7 elasticsearch = ElasticsearchInstanceES7.create();
 
-    @Override
-    @Before
-    public void setUp() {
-        super.setUp();
-    }
-
-    @Override
-    protected ElasticsearchInstance elasticsearch() {
-        return elasticsearch;
-    }
+    private final ObjectMapper objectMapper = new ObjectMapperProvider().get();
 
     @Override
     protected IndicesAdapter indicesAdapter() {
-        final ObjectMapper objectMapper = new ObjectMapperProvider().get();
         return new IndicesAdapterES7(
                 elasticsearch.elasticsearchClient(),
                 new StatsApi(objectMapper),
@@ -42,16 +28,7 @@ public class IndicesES7IT extends IndicesIT {
     }
 
     @Override
-    protected Map<String, Object> createTemplateFor(String indexWildcard) {
-        final Map<String, Object> beforeMapping = ImmutableMap.of(
-                "_source", ImmutableMap.of("enabled", false),
-                "properties", ImmutableMap.of("message",
-                        ImmutableMap.of("type", "text")));
-
-
-        return ImmutableMap.of(
-                "template", indexWildcard,
-                "mappings", beforeMapping
-        );
+    protected ElasticsearchInstance elasticsearch() {
+        return elasticsearch;
     }
 }
