51320a97d0eb0368db6bbaeaefc7071fcf8bca3d
Dennis Oelkers <dennis@graylog.com>
2020-07-06 12:17:28 +0200

    Implement `SearchesAdapter` for ES7.
    This change is implementing the `SearchesAdapter` for ES7, creates bindings and implements the abstract integration test for it. It also includes the required classes to generate `ScrollResult` & `ScrollResult.ScrollChunk` instances.

--- graylog-storage-elasticsearch7/src/main/java/org/graylog/storage/elasticsearch7/Elasticsearch7Module.java
+++ graylog-storage-elasticsearch7/src/main/java/org/graylog/storage/elasticsearch7/Elasticsearch7Module.java
@@ -1,10 +1,12 @@
 package org.graylog.storage.elasticsearch7;
 
+import com.google.inject.assistedinject.FactoryModuleBuilder;
 import com.google.inject.binder.LinkedBindingBuilder;
 import org.graylog2.indexer.cluster.ClusterAdapter;
 import org.graylog2.indexer.counts.CountsAdapter;
 import org.graylog2.indexer.indices.IndicesAdapter;
 import org.graylog2.indexer.messages.MessagesAdapter;
+import org.graylog2.indexer.searches.SearchesAdapter;
 import org.graylog2.plugin.VersionAwareModule;
 
 import static org.graylog.storage.elasticsearch7.Elasticsearch7Plugin.SUPPORTED_ES_VERSION;
@@ -16,6 +18,9 @@ public class Elasticsearch7Module extends VersionAwareModule {
         bindForSupportedVersion(ClusterAdapter.class).to(ClusterAdapterES7.class);
         bindForSupportedVersion(IndicesAdapter.class).to(IndicesAdapterES7.class);
         bindForSupportedVersion(MessagesAdapter.class).to(MessagesAdapterES7.class);
+        bindForSupportedVersion(SearchesAdapter.class).to(SearchesAdapterES7.class);
+
+        install(new FactoryModuleBuilder().build(ScrollResultES7.Factory.class));
     }
 
     private <T> LinkedBindingBuilder<T> bindForSupportedVersion(Class<T> interfaceClass) {
